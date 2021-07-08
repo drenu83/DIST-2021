0005088e94f4ab613fdce95d307ee489360e69f9
Rinesa Muharremi <rinesa.muharremi00@gmail.com>
2021-07-08 20:27:06 +0200

    Importimi i paketave dhe deklarimi i variablave

---
+++ AfatiEntity.java
@@ -0,0 +1,10 @@
+package Projekti;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+public class AfatiEntity {
+	private int ID;
+
+    private String afati;
+	
+	private LocalDate start_Date; 
+		
